36cd5129823898943ca1e6a7422d215cf908695b
tanglei <mac@tanglei.name>
2016-02-26 23:31:10 +0800

    add deploy shell

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-rm -rf .deploy/*
-cp -R _site/ .deploy/
-cd .deploy
-git add -A .
-git commit -m '$1'
-git push -u origin master:gh-pages --force
-cd ../
+while getopts "m:p" opt 
+do
+    case "$opt" in
+    "m")
+        echo "add comment: $OPTARG"
+        rm -rf .deploy/*
+        cp -R _site/ .deploy/
+        cd .deploy
+        git add -A .
+        git commit -m "$OPTARG"
+        cd ../
+        ;;
+    "p")
+        echo "push: $OPTARG"
+        git push -u origin master:gh-pages --force
+        ;;
+    *)
+        echo "-m \"commit msg\" -p (means push)"
+        ;;
+    esac
+done
